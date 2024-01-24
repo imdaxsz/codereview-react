0397b1bb197446352319e72e532faead4681b5c5
mihee <s2809h29@gmail.com>
2024-01-24 22:12:40 +0900

    feat: 각 페이지 Head 추가

--- src/pages/Booking.tsx
+++ src/pages/Booking.tsx
@@ -1,6 +1,9 @@
+import Head from '@components/Head';
+
 export default function Booking() {
   return (
     <div>
+      <Head title="예매하기 | Exhibition" />
     </div>
   );
 }
--- src/pages/Wishlist.tsx
+++ src/pages/Wishlist.tsx
@@ -1,6 +1,9 @@
+import Head from "@components/Head";
+
 export default function Wishlist() {
   return (
     <div>
+      <Head title="찜목록 | Exhibition" />
     </div>
   );
 }
