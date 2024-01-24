c70562f1b9fc7ffccef4a0153b3cdb70015c1eab
mihee <s2809h29@gmail.com>
2024-01-25 00:35:46 +0900

    feat: Exhibition 타입 정의 추가

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+export interface Exhibition {
+  id: number;
+  title: string;
+  imageUrl: string;
+  place: string;
+  price: number;
+  date: {
+    started: string;
+    ended: string;
+  };
+}
