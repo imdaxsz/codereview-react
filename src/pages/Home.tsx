4a83d857213292ec5bc0f83c8f9e5b6275bc96ac
mihee <s2809h29@gmail.com>
2024-01-26 00:14:36 +0900

    feat: 전시회 목록 조회 로딩 Loader 컴포넌트 적용

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,7 @@ import styles from '@styles/list.module.css';
 import ListItem from '@components/ListItem';
 import useExhibitions from '@hooks/useExhibitions';
 import Empty from '@components/Empty';
+import Loader from '@components/Loader';
 
 export default function Home() {
   const { exhibitions, isLoading } = useExhibitions();
@@ -11,7 +12,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head />
-      {isLoading && '로딩중'}
+      {isLoading && <Loader />}
       {exhibitions.length === 0 && !isLoading && <Empty message="예매 가능한 전시회가 없습니다" />}
       {exhibitions.map((item) => (
         <ListItem item={item} key={item.id} />
