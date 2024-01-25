54b6ebedcaea0c6d33b6a8116a9e1037df299e9b
mihee <s2809h29@gmail.com>
2024-01-26 00:22:20 +0900

    feat: 찜목록 페이지 퍼블리싱 및 데이터 바인딩

--- src/pages/Wishlist.tsx
+++ src/pages/Wishlist.tsx
@@ -1,9 +1,25 @@
-import Head from "@components/Head";
+import Empty from '@components/Empty';
+import Head from '@components/Head';
+import ListItem from '@components/ListItem';
+import { useAppSelector } from '@stores/index';
+import { wishlist } from '@stores/wishlistSlice';
+import styles from '@styles/list.module.css';
 
 export default function Wishlist() {
+  const list = useAppSelector(wishlist);
+
   return (
-    <div>
+    <div className={styles.container}>
       <Head title="찜목록 | Exhibition" />
+      {list.length === 0 && (
+        <Empty
+          message="찜해놓은 전시회가 없습니다"
+          suggestion="맘에 드는 전시회가 있다면 찜해보세요"
+        />
+      )}
+      {list.map((item) => (
+        <ListItem item={item} key={item.id} />
+      ))}
     </div>
   );
 }
