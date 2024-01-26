2a6e04569b048eb02cf8476ace25214de6f84bc5
mihee <s2809h29@gmail.com>
2024-01-26 20:42:27 +0900

    feat: 찜 추가/삭제 구현

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,3 +1,4 @@
+import useWishlistItem from '@hooks/useWishlistItem';
 import { Star } from '@phosphor-icons/react';
 import styles from '@styles/list.module.css';
 import { Link } from 'react-router-dom';
@@ -8,20 +9,25 @@ interface Props {
 }
 
 export default function ListItem({ item: { id, title, imageUrl, place, price, date } }: Props) {
+  const { isWishlistItem, toggleWishlistItem } = useWishlistItem({ id });
+
   return (
     <div className={styles.item__container}>
       <img className={styles.item__thumnail} src={imageUrl} alt="img" />
       <div className={styles.item__info}>
         <h3 className={styles.item__title}>{title}</h3>
         <p className={styles.item__place}>{place}</p>
-        <p className={styles.item__price}>{price.toLocaleString("ko")} 원</p>
+        <p className={styles.item__price}>{price.toLocaleString('ko')} 원</p>
         <span className={styles.item__date}>
           {date.started} ~ {date.ended}
         </span>
       </div>
       <div className={styles.item__action}>
-        <button name="찜하기">
-          <Star size={18} />
+        <button
+          name="찜하기"
+          onClick={() => toggleWishlistItem({ id, title, imageUrl, place, price, date })}
+        >
+          <Star size={18} weight={isWishlistItem ? 'fill' : 'regular'} />
         </button>
         <Link to={`/booking/${id}`}>예매하기</Link>
       </div>
