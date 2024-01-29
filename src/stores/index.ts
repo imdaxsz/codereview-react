e1e427f57509a049364cafc6831d7b184c417dee
mihee <s2809h29@gmail.com>
2024-01-29 22:18:12 +0900

    feat: wishlist에 persist를 적용하여 새로고침 시에도 state가 휘발되지 않도록 수정

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,12 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import wishlistReducer from './wishlistSlice';
 
-export const store = configureStore({
-  reducer: {
-    wishlist: wishlistReducer,
-  },
+const persistConfig = {
+  key: 'exhibition',
+  storage,
+  whitelist: ['wishlist'],
+};
+
+const reducers = combineReducers({
+  wishlist: wishlistReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
