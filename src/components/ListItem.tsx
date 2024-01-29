5c814609031013f35af7cbf1cb5b8c1913f77d28
mihee <s2809h29@gmail.com>
2024-01-29 19:57:26 +0900

    fix: ListItem 스타일 수정

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,7 +1,7 @@
 import useWishlistItem from '@hooks/useWishlistItem';
 import { Star } from '@phosphor-icons/react';
 import styles from '@styles/list.module.css';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { Exhibition } from 'types';
 
 interface Props {
@@ -10,26 +10,32 @@ interface Props {
 
 export default function ListItem({ item: { id, title, imageUrl, place, price, date } }: Props) {
   const { isWishlistItem, toggleWishlistItem } = useWishlistItem({ id });
+  const { pathname } = useLocation()
+  const isWishlist = pathname === "/wishlist";
 
   return (
     <div className={styles.item__container}>
       <img className={styles.item__thumnail} src={imageUrl} alt="img" />
-      <div className={styles.item__info}>
-        <h3 className={styles.item__title}>{title}</h3>
-        <p className={styles.item__place}>{place}</p>
-        <p className={styles.item__price}>{price.toLocaleString('ko')} 원</p>
-        <span className={styles.item__date}>
-          {date.started} ~ {date.ended}
-        </span>
-      </div>
-      <div className={styles.item__action}>
-        <button
-          name="찜하기"
-          onClick={() => toggleWishlistItem({ id, title, imageUrl, place, price, date })}
-        >
-          <Star size={18} weight={isWishlistItem ? 'fill' : 'regular'} />
-        </button>
-        <Link to={`/booking/${id}`}>예매하기</Link>
+      <div className={styles.item__content}>
+        <div className={styles.item__info__wrapper}>
+          <div className={styles.item__info}>
+            <h3 className={styles.item__title}>{title}</h3>
+            <p className={styles.item__place}>{place}</p>
+            <p className={styles.item__price}>{price.toLocaleString('ko')} 원</p>
+          </div>
+          <button
+            aria-label="찜하기"
+            onClick={() => toggleWishlistItem({ id, title, imageUrl, place, price, date })}
+          >
+            <Star size={18} weight={isWishlistItem ? 'fill' : 'regular'} />
+          </button>
+        </div>
+        <div className={`${styles.item__footer} ${isWishlist && styles.reverse}`}>
+          <span className={styles.item__date}>
+            {date.started} ~ {date.ended}
+          </span>
+          <Link to={`/booking/${id}`}>예매하기</Link>
+        </div>
       </div>
     </div>
   );
