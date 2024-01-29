a289284378c2d6ed6fcdeec1aeba81ac6f151f77
mihee <s2809h29@gmail.com>
2024-01-29 20:17:22 +0900

    fix: 페이지 이동 시 스크롤 위치 초기화되도록 수정

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,16 @@
 import { Outlet, useLocation } from 'react-router-dom';
 import BottomNavigation from './BottomNavigation';
+import { useEffect } from 'react';
 
 export default function Layout() {
-  const pathname = useLocation().pathname.split('/')[1];
+  const location = useLocation();
+  const pathname = location.pathname.split('/')[1];
+
+  // 페이지 이동 시 스크롤 위치 초기화
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
   return (
     <div className='wrapper'>
       <Outlet />
