c76dc1c2babddc0ba36b193383e6f149d1c80103
mihee <s2809h29@gmail.com>
2024-01-30 15:28:23 +0900

    feat: 찜 목록 무한스크롤 조회 구현

--- src/pages/Wishlist.tsx
+++ src/pages/Wishlist.tsx
@@ -1,24 +1,26 @@
 import Empty from '@components/Empty';
 import Head from '@components/Head';
 import ListItem from '@components/ListItem';
-import { useAppSelector } from '@stores/index';
+import Loader from '@components/Loader';
+import useWishlist from '@hooks/useWishlist';
 import styles from '@styles/list.module.css';
 
 export default function Wishlist() {
-  const list = useAppSelector((state) => state.wishlist.items);
-
+  const { target, wishlist, isLoading } = useWishlist();
   return (
     <div className={styles.container}>
       <Head title="찜목록 | Exhibition" />
-      {list.length === 0 && (
+      {isLoading && <Loader />}
+      {!isLoading && wishlist.length === 0 && (
         <Empty
           message="찜해놓은 전시회가 없습니다"
           suggestion="맘에 드는 전시회가 있다면 찜해보세요"
         />
       )}
-      {list.map((item) => (
+      {wishlist.map((item) => (
         <ListItem item={item} key={item.id} />
       ))}
+      <div ref={target} />
     </div>
   );
 }
